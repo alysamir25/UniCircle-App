89aa9263292506394e154c796c335a1e861548c6
Aly Elsharkawy <aly.elsharkawy25@gmail.com>
2026-02-07 06:56:20 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'solent-blue': '#2563EB',
+        'solent-gray': '#6B7280',
+      },
+    },
+  },
+  plugins: [],
+}
